5de6e1c3b7ca62c7c8ca70c0efd8cbdd4790d044
T-Deepthy <deeps1929@gmail.com>
2019-08-13 14:28:57 +0530

    Create login page Create signup page

---
+++ src/Login.js
@@ -0,0 +1,178 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable eqeqeq */
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+var apiBaseUrl = "http://localhost:4000/api/";
+class Login extends Component {
+  constructor(props){
+    super(props);
+    var localloginComponent=[];
+    localloginComponent.push(
+      <MuiThemeProvider key={"theme"}>
+        <div>
+         <TextField
+           hintText="Enter User Name"
+           floatingLabelText="User Name"
+           onChange={(event,newValue) => this.setState({username:newValue})}
+           />
+         <br/>
+           <TextField
+             type="password"
+             hintText="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+       </div>
+       </MuiThemeProvider>
+    )
+    this.state={
+      username:'',
+      password:'',
+      menuValue:1,
+      loginComponent:localloginComponent,
+      loginRole:'user'
+    }
+  }
+  componentWillMount(){
+  // console.log("willmount prop values",this.props);
+  if(this.props.role != undefined){
+    if(this.props.role == 'user'){
+      console.log("in user componentWillMount");
+      var localloginComponent=[];
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your User Name"
+             floatingLabelText="user Id"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+      this.setState({menuValue:1,loginComponent:localloginComponent,loginRole:'user'})
+    }
+    else if(this.props.role == 'teacher'){
+      console.log("in teacher componentWillMount");
+      var localloginComponent=[];
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your College Rollno"
+             floatingLabelText="Teacher Id"
+             onChange={(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange={(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+      this.setState({menuValue:2,loginComponent:localloginComponent,loginRole:'teacher'})
+    }
+  }
+  }
+  handleClick(event){
+    var payload={
+      "userid":this.state.username,
+	    "password":this.state.password,
+      "role":this.state.loginRole
+    }
+    axios.post(apiBaseUrl+'login', payload)
+  }
+  handleMenuChange(value){
+    console.log("menuvalue",value);
+    var loginRole;
+    if(value==1){
+      var localloginComponent=[];
+      loginRole='user';
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your College Rollno"
+             floatingLabelText="user Id"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+    }
+    else if(value == 2){
+      var localloginComponent=[];
+      loginRole='teacher';
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your College Rollno"
+             floatingLabelText="Teacher Id"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+    }
+    this.setState({menuValue:value,
+                   loginComponent:localloginComponent,
+                   loginRole:loginRole})
+  }
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+        <AppBar
+             title="Login"
+           />
+        </MuiThemeProvider>
+        {this.state.loginComponent}
+      </div>
+    );
+  }
+}
+
+const style = {
+  margin: 15,
+};
+
+export default Login;
---
+++ src/Loginscreen.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import Login from './Login';
+import SignUp from './SignUp';
+
+const style = {
+  margin: 15,
+};
+
+class Loginscreen extends Component {
+  constructor(props){
+    super(props);
+    var loginButtons=[];
+    loginButtons.push(
+      <div key={"Login-Div"}>
+      <MuiThemeProvider>
+        <div>
+           <RaisedButton label={"SignUp"} primary={true} style={style} onClick={(event) => this.handleClick(event,'student')}/>
+       </div>
+       </MuiThemeProvider>
+      </div>
+    )
+    this.state={
+      username:'',
+      password:'',
+      loginscreen:[],
+      loginmessage:'',
+      loginButtons:loginButtons,
+      userbuttonLabel:'Sign Up',
+      isLogin:true
+    }
+  }
+  componentWillMount(){
+    var loginscreen=[];
+    loginscreen.push(<Login parentContext={this} appContext={this.props.appContext} key={"LoginScreen"}/>);
+    var loginmessage = "New User?";
+    this.setState({
+                  loginscreen:loginscreen,
+                  loginmessage:loginmessage
+                    })
+  }
+  handleClick(event,userRole){
+    console.log("event",userRole);
+    var loginmessage;
+    if(this.state.isLogin){
+      let loginscreen=[];
+      loginscreen.push(<SignUp parentContext={this} appContext={this.props.appContext} role={userRole}/>);
+      loginmessage = "Already User? Go to Login";
+      let loginButtons=[];
+      loginButtons.push(
+        <div key="login-button">
+        <MuiThemeProvider>
+          <div>
+             <RaisedButton label={"Login"} primary={true} style={style} onClick={(event) => this.handleClick(event,userRole)}/>
+         </div>
+         </MuiThemeProvider>
+        </div>
+      )
+      this.setState({
+                     loginscreen:loginscreen,
+                     loginmessage:loginmessage,
+                     loginButtons:loginButtons,
+                     isLogin:false
+                   })
+    }
+    else{
+      let loginscreen=[],loginButtons=[];
+      loginButtons.push(
+        <div>
+        <MuiThemeProvider>
+          <div>
+             <RaisedButton label={"Sign Up"} primary={true} style={style} onClick={(event) => this.handleClick(event,'student')}/>
+         </div>
+         </MuiThemeProvider>
+        
+        </div>
+      )
+      loginscreen.push(<Login parentContext={this} appContext={this.props.appContext} role={userRole}/>);
+      loginmessage = "New User?";
+      this.setState({
+                     loginscreen:loginscreen,
+                     loginmessage:loginmessage,
+                     loginButtons:loginButtons,
+                     isLogin:true
+                   })
+    }
+  }
+  render() {
+    return (
+      <div className="loginscreen" key="loginscreen">
+        {this.state.loginscreen}
+        <div>
+          {this.state.loginmessage}
+          {this.state.loginButtons}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Loginscreen;
